eb6f9e1e564b2b4e7e7f8ba51780ed42baf57bc8
AlexandraTrifan <Alexandra.Trifan@analog.com>
2018-03-09 08:35:19 +0200

    Update build.sh

--- debian/build.sh
+++ debian/build.sh
@@ -4,6 +4,9 @@ echo "CURRENT "
 echo $(pwd)
 whoami
 CURRENT=$(pwd)
+echo $CURRENT
+ls
+echo "=============================================="
 cp ${CURRENT}/../libs/* debian/scopy/opt/scopy/lib/
 cp ${CURRENT}/build/scopy debian/scopy/opt/scopy/bin/
 ldd debian/scopy/opt/scopy/bin/scopy
